dc55c74fdbd45a0a121ec157c05f9130510a6ca5
Andrés Mejía <andmej@gmail.com>
2011-07-29 07:29:21 -0500

    Initial Commit

---
+++ Reto1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Reto1
+//
+//  Created by Andrés Mejía on 29/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
