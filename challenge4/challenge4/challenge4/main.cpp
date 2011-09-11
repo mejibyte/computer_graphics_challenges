d274e408d2c56cf5e730367394fd5bde318ad915
Andrés Mejía <andmej@gmail.com>
2011-09-11 13:54:56 -0500

    Adding rotation for all joints.

--- challenge4/challenge4/challenge4/main.cpp
+++ challenge4/challenge4/challenge4/main.cpp
@@ -8,46 +8,32 @@
 #include <algorithm>
 #include <cmath>
 
+using namespace std;
+
 GLfloat angle, fAspect;
-static int shoulder = 0, elbow = 0;
-
-void elbowAdd() {
-    elbow = (elbow + 5) % 360; 
-}
-
-void elbowSubtract() {
-    elbow = (elbow - 5) % 360;     
-}
-
-void shoulderAdd() {
-    shoulder = (shoulder + 5) % 360;     
-}
-
-void shoulderSubtract() {
-    shoulder = (shoulder - 5) % 360;     
-}
 
 // angles = { Y rotation at shoulder, Z rotation at shoulder, Z rotation at elbow, X rotation at wrist }
-int arm_angles[4] = {0, 0, 0, 0};
-enum { SHOULDER_Y, SHOULDER_Z, ELBOW_Z, WRIST_X };
+int arm_angles[5] = {0, 0, 0, 0, 0};
+enum { SHOULDER_Y, SHOULDER_Z, ELBOW_Z, WRIST_X, WRIST_Z };
 
-void change_angle(int angle, int delta) {
+void change_angle(int angle, int delta, int minimum = 0, int maximum = 180) {
     arm_angles[angle] = (arm_angles[angle] + delta) % 360;
+    arm_angles[angle] = max(arm_angles[angle], minimum);
+    arm_angles[angle] = min(arm_angles[angle], maximum);
 }
 
 // Desplegar la escena 3D
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     
-    glColor3f(1.0f, 1.0f, 1.0f);
-    
-    glPushMatrix();    
-        //glRotatef((GLfloat) arm_angles[SHOULDER_Y], 0., 1., 0.);
+    glPushMatrix();
+        glRotatef((GLfloat) arm_angles[SHOULDER_Y], 0., 1., 0.);
         glRotatef((GLfloat) arm_angles[SHOULDER_Z], 0., 0., 1.);
     
         glTranslatef(1., 0., 0.);
         glPushMatrix();
             glScalef(2.0, 0.4, 1.0);
+            glColor3f(1.0f, 1.0f, 1.0f);
             glutWireCube(1.);       // shoulder
         glPopMatrix();
 
@@ -56,16 +42,20 @@ void display(void) {
         glTranslatef(1., 0., 0.);
         glPushMatrix();
             glScalef(2.0, 0.4, 1.0);
+            glColor3f(249./255., 238./255., 153./255.);
             glutWireCube(1.);       // elbow
         glPopMatrix();
     
-//        glTranslatef(1., 0., 0.);
-//        glRotatef((GLfloat) arm_angles[WRIST_X], 1., 0., 0.);
-//        glTranslatef(1., 0., 0.);
-//        glPushMatrix();
-//            glScalef(2.0, 0.4, 1.0);
-//            glutWireCube(1.);       // wrist
-//        glPopMatrix();
+        glTranslatef(1., 0., 0.);
+        glRotatef((GLfloat) arm_angles[WRIST_Z], 0., 0., 1.);
+        glRotatef((GLfloat) arm_angles[WRIST_X], 1., 0., 0.);
+
+        glTranslatef(1., 0., 0.);
+        glPushMatrix();
+            glScalef(2.0, 0.4, 1.0);
+            glColor3f(207./255., 106./255., 76./255.);
+            glutWireCube(1.);       // wrist
+        glPopMatrix();
     glPopMatrix();
     
     // Ejecutar los comandos 
@@ -80,7 +70,7 @@ void visualizationParams(void) {
 	glLoadIdentity();
     
 	// Especifica proyeccion perspectiva
-	gluPerspective(angle,fAspect,0.1,500);
+	gluPerspective(45,fAspect,0.1,500);
     
 	// Especifica el sistema de coordenadas del modelo
 	glMatrixMode(GL_MODELVIEW);
@@ -89,7 +79,7 @@ void visualizationParams(void) {
     
 	// Especifica la posicion del observador
 	// Posicion del ojo, hacia donde mira, vector arriba
-	gluLookAt(0,0,15, 0,0,0, 0,1,0);
+	gluLookAt(0,0,18, 0,0,0, 0,1,0);
 }
 
 // Se llama cuando cambia el tamaño de la ventana
@@ -110,38 +100,54 @@ void reshape(GLsizei w, GLsizei h) {
 void specialKeys(int key, int x, int y)
 {
     if(key == GLUT_KEY_UP) {
-        elbowAdd();
     }
     if(key == GLUT_KEY_DOWN) {
-        elbowSubtract();
     }
     if(key == GLUT_KEY_LEFT) {
-        shoulderAdd();
     }
     if(key == GLUT_KEY_RIGHT) {
-        shoulderSubtract();
     }
     glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int x, int y) {
+    int delta = 9;
+
     switch (key) {
-        case 27:
+        case 27: //ESC
             exit(0);
+        case 'q':
+            change_angle(SHOULDER_Y, delta, -180, 0);
             break;
-        case 'w':		
-            display();
+        case 'a':
+            change_angle(SHOULDER_Y, -delta, -180, 0);
             break;
-        case 's':		
-            display();            
+        case 'w':
+            change_angle(SHOULDER_Z, delta, -90, 90);
             break;
-        case 'a':		
-            display();
+        case 's':
+            change_angle(SHOULDER_Z, -delta, -90, 90);
             break;
-        case 'd':		
-            display();
+        case 'e':
+            change_angle(ELBOW_Z, delta, 0, 135);
+            break;
+        case 'd':
+            change_angle(ELBOW_Z, -delta, 0, 135);
+            break;
+        case 'r':
+            change_angle(WRIST_X, delta, -90, 90);
+            break;
+        case 'f':
+            change_angle(WRIST_X, -delta, -90, 90);
+            break;
+        case 't':
+            change_angle(WRIST_Z, delta, -15, 90);
+            break;
+        case 'g':
+            change_angle(WRIST_Z, -delta, -15, 90);
             break;
     }
+    glutPostRedisplay();
 }
 
 
@@ -154,7 +160,7 @@ int main(int argc, char * argv[]) {
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutSpecialFunc(specialKeys);
-    glutKeyboardFunc(keyboard);    
+    glutKeyboardFunc(keyboard);
 
     // Inicialización
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Negro
